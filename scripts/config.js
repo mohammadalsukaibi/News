31b2cdfa0c063c613082344d400c270c44ad2780
mohammadalsukaibi <46539624+mohammadalsukaibi@users.noreply.github.com>
2021-12-08 22:27:13 +0300

    Add files via upload

---
+++ scripts/config.js
@@ -0,0 +1,5 @@
+let config = {
+    google_maps_api_key : 'AIzaSyDcETWUYJbLUQR5rGH0iG41HXuXfMVnNdw',
+    news_api_key : '2ad8ff53ee0a45739e35562d7f967bc2',
+    weather_api_key : '24a5169de8394568b05721c5e36a00a4',
+}
